7de5b5edf4d2354c94a50e6cdf04513e7e2b3b0d
Ga Mo <oni221ok@gmail.com>
2024-12-20 00:35:07 +0700

    combobox winform

---
+++ comboboxgui/comboboxgui/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace comboboxgui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbb1 = new System.Windows.Forms.ComboBox();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.cblop = new System.Windows.Forms.ComboBox();
+            this.cbhs = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbb1
+            // 
+            this.cbb1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbb1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbb1.Font = new System.Drawing.Font("JetBrains Mono NL", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbb1.FormattingEnabled = true;
+            this.cbb1.Location = new System.Drawing.Point(110, 51);
+            this.cbb1.Name = "cbb1";
+            this.cbb1.Size = new System.Drawing.Size(199, 33);
+            this.cbb1.TabIndex = 0;
+            this.cbb1.SelectedValueChanged += new System.EventHandler(this.cbb1_SelectedValueChanged);
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(23, 51);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(35, 13);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "label1";
+            // 
+            // cblop
+            // 
+            this.cblop.FormattingEnabled = true;
+            this.cblop.Location = new System.Drawing.Point(26, 199);
+            this.cblop.Name = "cblop";
+            this.cblop.Size = new System.Drawing.Size(121, 21);
+            this.cblop.TabIndex = 2;
+            this.cblop.SelectedValueChanged += new System.EventHandler(this.cblop_SelectedValueChanged);
+            // 
+            // cbhs
+            // 
+            this.cbhs.FormattingEnabled = true;
+            this.cbhs.Location = new System.Drawing.Point(210, 199);
+            this.cbhs.Name = "cbhs";
+            this.cbhs.Size = new System.Drawing.Size(121, 21);
+            this.cbhs.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(421, 483);
+            this.Controls.Add(this.cbhs);
+            this.Controls.Add(this.cblop);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.cbb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbb1;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.ComboBox cblop;
+        private System.Windows.Forms.ComboBox cbhs;
+    }
+}
+
---
+++ comboboxgui/comboboxgui/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comboboxgui
+{
+    public partial class Form1 : Form
+    {
+        List<dienthoai> listdata;
+        List<Lop> lops;
+        public Form1()
+        {
+            InitializeComponent();
+
+            listdata = new List<dienthoai>() {
+                new dienthoai() { hang = "iphone", gia = 1500 },
+                new dienthoai() { hang = "google", gia = 2000 }
+            };
+
+            cbb1.DataSource = listdata; // truyền datasource vào để sử dụng selectedvalue
+            cbb1.DisplayMember = "hang";
+
+            Addbinding();
+
+
+            // add thông tin vào cblop.datasource
+            lops = new List<Lop>();
+            lops.Add(new Lop() { 
+                lop = "gnm1", 
+                hs = new List<string> { "thiet", "hau", "khanh"} 
+            });
+            lops.Add(new Lop()
+            {
+                lop = "gnm2",
+                hs = new List<string> { "phat", "phuc", "pin" }
+            });
+
+            cblop.DataSource = lops;
+            cblop.DisplayMember = "lop";
+        }
+
+        //private void cbb1_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //ComboBox cbb = sender as ComboBox;
+        // nếu không chọn gì thì index của cbb == -1,
+        // start index = 0;
+        //if (cbb.SelectedIndex != -1)
+        //{
+        //lb1.Text = (cbb.DisplayMember = "hang").ToString();
+        // selecteditem.tostring() trả về text của choice
+        //}
+        //}
+
+
+
+        //private void cbb1_Click(object sender, EventArgs e)
+        //{
+        //    ComboBox cbb = sender as ComboBox;
+        //    listdata = new List<dienthoai>() { 
+        //        new dienthoai() { hang = "iphone", gia = 1500 },
+        //        new dienthoai() { hang = "google", gia = 2000 }
+        //    };
+
+        //    cbb.DataSource = listdata; // truyền datasource vào để sử dụng selectedvalue
+        //    cbb.DisplayMember = "hang";
+        //}
+
+        void Addbinding()
+        {
+            lb1.DataBindings.Add(new Binding("Text", cbb1.DataSource, "hang"));
+        }
+
+        private void cbb1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cbb = sender as ComboBox;
+
+            if (cbb.SelectedValue != null)
+            {
+                dienthoai dt = cbb.SelectedValue as dienthoai;
+                lb1.Text = dt.hang.ToString();
+            }
+        }
+
+        private void cblop_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+
+            if (cblop.SelectedValue != null)
+            {
+                Lop lopss = cblop.SelectedValue as Lop;
+                cbhs.DataSource = lopss.hs;
+            }
+        }
+    }
+
+    public class dienthoai
+    {
+        public string hang { get; set; }
+        public int gia { get; set; }
+    }
+    public class Lop
+    {
+        public string lop { get; set; }
+        public List<string> hs { get; set; }
+    }
+}
