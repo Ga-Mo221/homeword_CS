0c9324d1af80f71930e8e546e80e7578c5cfbf77
Ga Mo <oni221ok@gmail.com>
2024-12-20 07:52:20 +0700

    PictureBox wimform

---
+++ PictureBoxGUI/PictureBoxGUI/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PictureBoxGUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbimg = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbimg
+            // 
+            this.cbimg.FormattingEnabled = true;
+            this.cbimg.Location = new System.Drawing.Point(12, 12);
+            this.cbimg.Name = "cbimg";
+            this.cbimg.Size = new System.Drawing.Size(121, 21);
+            this.cbimg.TabIndex = 0;
+            this.cbimg.SelectedValueChanged += new System.EventHandler(this.cbimg_SelectedValueChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 461);
+            this.Controls.Add(this.cbimg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbimg;
+    }
+}
+
---
+++ PictureBoxGUI/PictureBoxGUI/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxGUI
+{
+    public partial class Login : Form
+    {
+        PictureBox Piclogin;
+        String duoi = ".PNG";
+
+        public Login()
+        {
+            InitializeComponent();
+            Addpicturebox();
+            lay_ten_image();
+        }
+        private void Addpicturebox()
+        {
+            Piclogin = new PictureBox()
+            {
+                Size = new Size(200, 100),
+                Location = new Point(200, 60),
+            };
+            
+            this.Controls.Add(Piclogin);
+        }
+
+        void lay_ten_image()
+        {
+            List<string> listimage = new List<string>() { "nhin1","nhin2", "nhin3", "nhin4" };
+            cbimg.DataSource = listimage;
+        }
+
+        /*
+         * tạo một thư mục để chứa ảnh chuột phải vào PictureBoxGUI chọn add -> New fodel
+         * kéo thả ảnh vào fodel vừa tạo
+         * để dùng được ảnh trong Bitmap thì kích chuột phải vào PictureBoxGUI(tên project) 
+         * chọn Open fodel in File Explorer
+         * sao chép fodel chứa ảnh vào bin -> Debug
+         */
+        private void cbimg_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+
+            if (cb.SelectedValue != null)
+            {
+                Bitmap bm = new Bitmap(Application.StartupPath + "\\photo\\" + cb.SelectedValue.ToString() + duoi);
+                Piclogin.Image = bm;
+            }
+        }
+    }
+}
